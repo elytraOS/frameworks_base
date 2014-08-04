7ebe3312566aa3a590e2cc03cb91d5d9327015f5
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (2/2).
    Adapted to Lineage SDK and squashed with the following changes:
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 10:40:41 -0700
    
          Keyguard: Don't disable visibility when scrambling pin.
    
            Otherwise the first numbers text is flipped to invisible
            even though it shouldn't be.
    
          Change-Id: Ia2b80d594e7f11af7a60a85bdb4ea9909d0ac20b
    
      From: Adnan Begovic <adnan@cyngn.com>
      Date: Wed, 05 Aug 2015 16:21:27 -0700
    
          Keyguard: Fix scramblepin logic.
    
          Change-Id: I71e6d26e853fa7bfaeea4b3256902881ac7f74fc
    
      From: LuK1337 <priv.luk@gmail.com>
      Date: Sat Apr 24 18:14:46 2021 +0200
    
          Keyguard: Fix mapping mismatch between KeyEvent and NumPadKey
    
          Change-Id: Ida28a4f037c2f3fa843ac136779e026020d711db
    
    Original-Change-Id: Ibe8611eb0b8a5bd9275bc96682d2cbafaa37d8f5
    Change-Id: I6716b1c76b69d51bd1457ce07de2274541fca1e8
    
    -----
    
    SystemUI: Implement lockscreen quick unlock.
    
    [BigBrother1984 <carlosavignano@aospa.co>]:
        * PasswordTextView QuickUnlock listener functions and method
    
    [mydongistiny <jaysonedson@gmail.com>]:
        * Forward port to Android 8 / 10
    
    [SagarMakhar <sagarmakhar@gmail.com>]:
        * Forward port to Android 12
    
    [Hikari-no-Tenshi <kyryljan.serhij@gmail.com>]:
        * Make Quick Unlock compatible with long PIN/Password (Idea from OOS)
        * Use AsyncTask for PIN/Password check.
        * Restore call on main thread check.
    
    [Hernán Castañón <herna@paranoidandroid.co>]:
        * Make it independent of toggle (enabled by default)
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]:
        * Code cleanup and format
        * Hide Ok button in favour of quick unlock
        * Move backspace button to right
    
    [ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>]:
        * Simplify and refactor to use verifyPasswordAndUnlock()
        * Handle case where password length is not set
        * Hide backspace button when PIN is empty
        * Adapt to 12L - thanks to Jyotiraditya
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Matt Mower <mowerm@gmail.com>
    Co-authored-by: BigBrother1984 <carlosavignano@aospa.co>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I1d721ad454fe4a37868071ef978468a12c844a03

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5639,6 +5639,13 @@ public final class Settings {
         public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
 
         /**
+         * Whether to scramble a pin unlock layout
+         * 0 = 0ff, 1 = on
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT = "lockscreen_scramble_pin_layout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
