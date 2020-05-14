46fd60f70f657d6b51920c2b82407c1779a929b1
Anushek Prasal <anushekprasal@gmail.com>
2020-05-14 23:20:31 +0530

    SystemUI: Improve Network Traffic
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Ic5a17979f9adbe761c8c5e3d5130004a90430daf
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconControllerImpl.java
@@ -479,12 +479,6 @@ public class StatusBarIconControllerImpl extends StatusBarIconList implements Tu
         return s;
     }
 
-    public void onPanelExpanded(boolean isExpanded) {
-        for (IconManager manager : mIconGroups) {
-            manager.onPanelExpanded(isExpanded);
-        }
-    }
-
     public void setKeyguardShowing(boolean showing) {
         for (IconManager manager : mIconGroups) {
             manager.setKeyguardShowing(showing);
