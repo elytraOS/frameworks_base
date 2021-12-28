19482398f18d0711110cf0b7d2cf3d2f694be131
SagarMakhar <sagarmakhar@gmail.com>
2021-12-28 20:02:43 +0530

    base: Brightness curve fix for realme devices
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I83b38a1334b8df156e4e53b06243c48d565be5d5
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -16,10 +16,6 @@
 
 package com.android.systemui.settings.brightness;
 
-import static com.android.settingslib.display.BrightnessUtils.GAMMA_SPACE_MAX;
-import static com.android.settingslib.display.BrightnessUtils.convertGammaToLinearFloat;
-import static com.android.settingslib.display.BrightnessUtils.convertLinearToGammaFloat;
-
 import android.animation.ValueAnimator;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -293,7 +289,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         mContext = context;
         mIcon = icon;
         mControl = control;
-        mControl.setMax(GAMMA_SPACE_MAX);
+        mControl.setMax(255);
         mBackgroundHandler = bgHandler;
         mUserTracker = new CurrentUserTracker(broadcastDispatcher) {
             @Override
@@ -370,14 +366,16 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
             maxBacklight = mBrightnessMax;
         }
         final float valFloat = MathUtils.min(
-                convertGammaToLinearFloat(value, minBacklight, maxBacklight),
-                maxBacklight);
+                MathUtils.constrain(BrightnessSynchronizer.brightnessIntToFloat(value),
+                         minBacklight, maxBacklight),
+                         maxBacklight);
         if (stopTracking) {
             // TODO(brightnessfloat): change to use float value instead.
             MetricsLogger.action(mContext, metric,
                     BrightnessSynchronizer.brightnessFloatToInt(valFloat));
 
         }
+android.util.Slog.d("sagar", "val=" + value + ", valFloat=" + valFloat);
         setBrightness(valFloat);
         if (!tracking) {
             AsyncTask.execute(new Runnable() {
@@ -437,15 +435,16 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
             min = mBrightnessMin;
             max = mBrightnessMax;
         }
-        // convertGammaToLinearFloat returns 0-1
         if (BrightnessSynchronizer.floatEquals(brightnessValue,
-                convertGammaToLinearFloat(mControl.getValue(), min, max))) {
+                    MathUtils.constrain(BrightnessSynchronizer.brightnessIntToFloat(mControl.getValue()),
+                            min, max))) {
             // If the value in the slider is equal to the value on the current brightness
             // then the slider does not need to animate, since the brightness will not change.
             return;
         }
         // Returns GAMMA_SPACE_MIN - GAMMA_SPACE_MAX
-        final int sliderVal = convertLinearToGammaFloat(brightnessValue, min, max);
+        final int sliderVal = BrightnessSynchronizer.brightnessFloatToInt(
+                MathUtils.constrain(brightnessValue, min, max));
         animateSliderTo(sliderVal);
     }
 
@@ -469,7 +468,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
             mExternalChange = false;
         });
         final long animationDuration = SLIDER_ANIMATION_DURATION * Math.abs(
-                mControl.getValue() - target) / GAMMA_SPACE_MAX;
+                mControl.getValue() - target) / 255;
         mSliderAnimator.setDuration(animationDuration);
         mSliderAnimator.start();
     }
