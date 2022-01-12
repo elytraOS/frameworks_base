ac9e75c6984468ea1476345fda99b458be92f91e
Ido Ben-Hur <idoybh2@gmail.com>
2022-01-12 14:24:58 +0200

    SystemUI: InternetTile: We should be secure as well
    Change-Id: Ie1cdf82f40161f9d1ad5a9591aa3ca358528fa87

--- packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/InternetTile.java
@@ -61,6 +61,7 @@ import com.android.systemui.statusbar.connectivity.NetworkController;
 import com.android.systemui.statusbar.connectivity.SignalCallback;
 import com.android.systemui.statusbar.connectivity.WifiIcons;
 import com.android.systemui.statusbar.connectivity.WifiIndicators;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
