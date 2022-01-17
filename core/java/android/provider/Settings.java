3a59324dec2d003b766572e66d038615f7d4ae2c
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:20:04 +0530

    base: Allow toggling camera/mic privacy indicator [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ie26639999e1a76a8224921ed36c199154d13e247

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11019,6 +11019,12 @@ public final class Settings {
         public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
 
         /**
+         * Whether to show privacy indicator for camera
+         * @hide
+         */
+        public static final String ENABLE_CAMERA_PRIVACY_INDICATOR = "enable_camera_privacy_indicator";
+
+        /**
          * Accurate shades (default 1)
          * @hide
          */
