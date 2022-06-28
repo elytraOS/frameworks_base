8cd4e863b2bd3ce4a0d1044b9a7a11411ed9d049
Rasenkai <rasenkai99@gmail.com>
2022-06-28 16:45:09 +0200

    Revert "base: Global Actions with Advanced Reboot [1/2]"
    This reverts commit df511c961008264e7a5d4889cc5408c6b9345675.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5309,13 +5309,7 @@ public final class Settings {
         /**
          * @hide
          */
-
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
-
-        /**
-         * @hide
-         */
-        public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
 
         /**
          * Double tap on lockscreen to sleep
@@ -10475,11 +10469,6 @@ public final class Settings {
          */
         @Readable
         public static final String NOTIFICATION_DISMISS_RTL = "notification_dismiss_rtl";
-
-        /**
-         * @hide
-         */
-        public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
          * Comma separated list of QS tiles that have been auto-added already.
