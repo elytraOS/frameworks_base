e4202745ffc768dde99755c5dc676ee79dcf60ff
hupeng3 <hp121520@gmail.com>
2022-08-22 11:04:20 +0800

    Fix NPE in ProcessState
    Bug: 233119628
    Test: atest ProcStatsValidationTests
    Test: atest ProcessStatsDumpsysTest
    Test: Manual - dumpsys procstats -a
    
    The state of Uidstate is sometimes null, and a
    judgment condition needs to be added.
    
    Signed-off-by: hupeng3 <hp121520@gmail.com>
    Change-Id: Ib2ca22e886e46c9e932d2c0edb1af2fbf0350296

--- core/java/com/android/internal/app/procstats/ProcessState.java
+++ core/java/com/android/internal/app/procstats/ProcessState.java
@@ -473,7 +473,7 @@ public final class ProcessState {
                 }
             }
             mCurCombinedState = state;
-            final UidState uidState = mStats.mUidStates.get(mUid);
+            UidState uidState = mStats.mUidStates.get(mUid);
             if (uidState != null) {
                 uidState.updateCombinedState(state, now);
             }
