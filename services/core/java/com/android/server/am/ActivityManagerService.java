1a31772ef6831e84c6c1ad88ba4a475915ee3a24
strwbrry143 <alexfinhart@gmail.com>
2022-08-23 13:30:20 +0800

    ActiveServices: Port CLO's service reschedule
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -419,6 +419,7 @@ import com.android.server.utils.TimingsTraceAndSlog;
 import com.android.server.vr.VrManagerInternal;
 import com.android.server.wm.ActivityMetricsLaunchObserver;
 import com.android.server.wm.ActivityServiceConnectionsHolder;
+import com.android.server.wm.ActivityTaskSupervisor;
 import com.android.server.wm.ActivityTaskManagerInternal;
 import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerInternal;
@@ -590,6 +591,9 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     private Installer mInstaller;
 
+    /** Run all ActivityStacks through this */
+    ActivityTaskSupervisor mTaskSupervisor;
+    
     final InstrumentationReporter mInstrumentationReporter = new InstrumentationReporter();
 
     @CompositeRWLock({"this", "mProcLock"})
@@ -2466,6 +2470,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         mActivityTaskManager.initialize(mIntentFirewall, mPendingIntentController,
                 DisplayThread.get().getLooper());
         mAtmInternal = LocalServices.getService(ActivityTaskManagerInternal.class);
+        mTaskSupervisor = mActivityTaskManager.mTaskSupervisor;
 
         mHiddenApiBlacklist = new HiddenApiSettings(mHandler, mContext);
         mSdkSandboxSettings = new SdkSandboxSettings(mContext);
